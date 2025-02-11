7874cddf29e9be8b92cd4888141fd8be5196dc71
Sara Maged <saraahmedmaged@gmail.com>
2025-02-11 10:54:20 +0200

    initial commit

---
+++ src/main/java/com/example/user_attendance_service/UserAttendanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_attendance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAttendanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAttendanceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/user_attendance_service/UserAttendanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_attendance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAttendanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
