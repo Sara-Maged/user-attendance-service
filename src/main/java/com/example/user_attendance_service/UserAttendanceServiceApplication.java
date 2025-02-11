c71a47aec9d769678a1a9918f9807b767f061746
Sara Maged <saraahmedmaged@gmail.com>
2025-02-11 17:58:12 +0200

    register client

--- src/main/java/com/example/user_attendance_service/UserAttendanceServiceApplication.java
+++ src/main/java/com/example/user_attendance_service/UserAttendanceServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.user_attendance_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserAttendanceServiceApplication {
 
 	public static void main(String[] args) {
